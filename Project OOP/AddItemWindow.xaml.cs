f194449f48ac7e2e5bce16fade7d8afe173ee9d2
DEWAELETIM\timde <tim.dewaele@student.vives.be>
2024-04-18 21:46:42 +0200

    De 3 WPF zijn aangemaakt, via de verwelkomings WPF kan je naar de WPF gaan om een item toe te en kan je ook naar de WPF waar alle items gaan staan. Je kan ook van de WPF waar alle items staat en de WPF om items toe te voegen terug gaan naar de verwelkomings WPF via een knop.

---
+++ Project OOP/AddItemWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_OOP
+{
+    /// <summary>
+    /// Interaction logic for AddItemWindow.xaml
+    /// </summary>
+    public partial class AddItemWindow : Window
+    {
+        public AddItemWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTerug_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true; // Stel het dialoogresultaat in op true
+            Close(); // Sluit het huidige venster
+        }
+    }
+}
